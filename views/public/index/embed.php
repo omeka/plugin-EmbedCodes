2a9914f892908ddbd4fa4612e67384aaeb270eaf
patrickmj <patrickmjchnm@gmail.com>
2013-04-12 18:14:18 -0400

    Hook to add content to the embedded stuff.
    Omeka Commons will need to use this to add links to Commons and the original site.

--- views/public/index/embed.php
+++ views/public/index/embed.php
@@ -9,10 +9,11 @@ echo head_css();
 <body>
 <div>
 <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
-
+<?php fire_plugin_hook('embed_codes_content', array('item'=>$item, 'view'=>$this)); ?>
 <?php if (metadata('item', 'has files')): ?>
     <div class="element-text"><?php echo files_for_item(); ?></div>
 <?php endif; ?>
+
 </div>
 </body>
 </html>
