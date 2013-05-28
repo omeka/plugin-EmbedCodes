a679d7ea53d12ba4a8fe7f010d81a0dd3c699420
patrickmj <patrickmjchnm@gmail.com>
2013-05-28 12:26:26 -0400

    Force embed to specific dimensions.

--- EmbedCodesPlugin.php
+++ EmbedCodesPlugin.php
@@ -43,7 +43,7 @@ class EmbedCodesPlugin extends Omeka_Plugin_AbstractPlugin
         
         $html = "<div id='embed-codes'><h2>Embed</h2>";
         $html .= "<p>Copy the code below into your web page</p>";
-        $iFrameHtml = "<iframe class='omeka-embed' src='$uri' frameborder='0' allowfullscreen></iframe>";
+        $iFrameHtml = "<iframe class='omeka-embed' src='$uri' width='560px' height='315px' frameborder='0' allowfullscreen></iframe>";
         $html .= "<textarea id='embed-code-text' style='font-family:monospace' rows='4'>$iFrameHtml</textarea>";
         $html .= "<script type='text/javascript'>";
         $html .= "document.getElementById('embed-code-text').select();";
