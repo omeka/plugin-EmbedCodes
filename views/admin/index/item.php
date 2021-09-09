c51d52e7da92f3c652f2fb973cf6c506c671b34c
Kim Nguyen <design@kimisgold.com>
2021-09-09 16:37:28 -0400

    Add responsive table wrappers.

--- views/admin/index/item.php
+++ views/admin/index/item.php
@@ -8,35 +8,34 @@ echo head(array('title'=>__("Embed Statistics for %s", metadata($item, array('Du
 
 <div class="pagination"><?php echo pagination_links(); ?></div>
 
-
-<table>
-    <thead>
-    <tr>
-        <?php
-        $browseHeadings[__('Site')] = null;
-        $browseHeadings[__('Link')] = null;
-        $browseHeadings[__('First viewed')] = 'first_view';
-        $browseHeadings[__('Last viewed')] = 'last_view';
-        $browseHeadings[__('Views')] = 'view_count';
-        echo browse_sort_links($browseHeadings, array('link_tag' => 'th scope="col"', 'list_tag' => '')); 
-        ?>    
-    </tr>
-    </thead>
-    <tbody>
-    
-    <?php foreach(loop('embed', $embeds) as $embed):?>
-    <?php $item = get_record_by_id('item', $embed->item_id); ?>
-    <tr>
-        <td><?php echo $embed->host; ?></td>
-        <td><a href="<?php echo $embed->url; ?>"><?php echo $embed->url; ?></a></td>
-        <td><?php echo metadata('embed', 'first_view'); ?></td>
-        <td><?php echo metadata('embed', 'last_view'); ?></td>
-        <td><?php echo metadata('embed', 'view_count'); ?></td>
-    </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
-
-
+<div class="table-responsive">
+    <table>
+        <thead>
+        <tr>
+            <?php
+            $browseHeadings[__('Site')] = null;
+            $browseHeadings[__('Link')] = null;
+            $browseHeadings[__('First viewed')] = 'first_view';
+            $browseHeadings[__('Last viewed')] = 'last_view';
+            $browseHeadings[__('Views')] = 'view_count';
+            echo browse_sort_links($browseHeadings, array('link_tag' => 'th scope="col"', 'list_tag' => '')); 
+            ?>    
+        </tr>
+        </thead>
+        <tbody>
+        
+        <?php foreach(loop('embed', $embeds) as $embed):?>
+        <?php $item = get_record_by_id('item', $embed->item_id); ?>
+        <tr>
+            <td><?php echo $embed->host; ?></td>
+            <td><a href="<?php echo $embed->url; ?>"><?php echo $embed->url; ?></a></td>
+            <td><?php echo metadata('embed', 'first_view'); ?></td>
+            <td><?php echo metadata('embed', 'last_view'); ?></td>
+            <td><?php echo metadata('embed', 'view_count'); ?></td>
+        </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 
 </div>
